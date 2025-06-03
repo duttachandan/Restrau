5e73f405338f41accdfd262240d199a06ada0602
duttachandan <duttaankit917@gmail.com>
2025-06-03 15:16:41 +0530

    updated next section

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,34 @@
 // Intializing AOS
-
 AOS.init();
 
-// JQuery Code
 
+// Swiper Js Code
+new Swiper(".spcl-dish-sm", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 1,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+// JQuery Code
 $("document").ready(() => {
   $(".nav-btn").click(() => {
     $(".navbar-collapse").slideToggle();
   });
 });
+
+// document.addEventListener("mousemove", (event) => {
+//   console.log(event.pageX, event.pageY);
+// });
